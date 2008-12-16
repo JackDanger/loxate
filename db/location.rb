a4ad36e7db6269d7c039d996cbe7865ca48a964b
Jack Danger Canty <gitcommit@6brand.com>
2008-12-16 01:32:55 -0800

    lots of logic to parse and save the address.  Need tests SOON.

--- db/location.rb
+++ db/location.rb
@@ -16,27 +16,55 @@ class Location < ActiveRecord::Base
 
   class << self
 
+    def find_or_create_by_user_entered_location(location)
+
+      nickname, address, coordinates = parse(location)
+
+      if !nickname.empty?
+        # see if this address is really just a nickname
+        current_location = find(:first, :conditions => "LOWER(locations.nickname) = #{address.downcase}")
+        address     = current_location.address
+        coordinates = current_location.coordinates
+      elsif !address.empty?
+        # see if this address has already been used
+        find(:first, :conditions => "LOWER(locations.address) = #{address.downcase}")
+      end
+
+      # lookup coordinates unless they've been provided
+      coordinates = geocoordinate(address) if coordinates.empty?
+
+      # if a nickname has been provided then make sure
+      # it doesn't get stuck with an old address
+      if current_location && !nickname.empty?
+        current_location.address = address
+        current_location.coordinates = coordinates
+      end
+
+      current_location ||= new(:coordinates => coordinates,
+                               :address => address,
+                               :nickname => nickname)
+      current_location.save
+      current_location
+    end
+
     def parse(location)
-      location = location.to_s
-      nickname, address, coordinates = '', '', ''
+      nickname, address, coordinates = '', location.to_s, ''
 
-      return '', '', '' if location =~ /^\s*$/
+      return '', '', '' if address =~ /^\s*$/
 
-      # if there is a nickname entered to name this location then extract it
-      if location =~ /(.*)\[(.*)\]$/
-        location, nickname = location.scan(/(.*)\[(.*)\]$/).flatten
+      # if there is a nickname entered to name this address then extract it
+      if address =~ /(.*)\[(.*)\]$/
+        address, nickname = address.scan(/(.*)\[(.*)\]$/).flatten
       end
 
       # if the user has passed us some coordinates then accept them as given
-      # otherwise look them up
-      coordinates = location.=~(/^\s*-?\d+\.\d+,-?\d+\.\d+\s*$/) ?
-        location : geocoordinate(location)
+      coordinates = address if address =~ /^\s*-?\d+\.\d+,-?\d+\.\d+\s*$/
 
-      [nickname, address, coordinates]
+      [nickname.strip, address.strip, coordinates.strip]
     end
 
     def geocoordinate(location)
-      csv = open("maps.google.com/maps/geo?q=#{location}&output=csv&sensor=false&key=#{GOOGLE_MAP_KEY}").read
+      csv = open("http://maps.google.com/maps/geo?q=#{location}&output=csv&sensor=false&key=#{GOOGLE_MAP_KEY}").read
       status, accuracy, c1, c2 = csv.to_s.split(',')
       "#{c1},#{c2}"
     end
