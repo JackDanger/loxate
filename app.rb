e13928d363aa2d593afca927e72869b7ef965bf0
Jack Danger Canty <gitcommit@6brand.com>
2008-12-16 00:32:45 -0800

    using a helper to find the email address from params.

--- app.rb
+++ app.rb
@@ -8,22 +8,34 @@ get '/' do
 end
 
 get '/:email' do
+  find_email
   # find and display location
 end
 
 post '/' do
+  find_email
   # save the location if the email is now
   # otherwise display error and ask for token
 end
 
 put '/' do
-  # update the location if the token is valid
+  find_email
 end
 
 get '/request_reset/:email' do
+  find_email
   # send an email to allow resetting this email's token
 end
 
 get '/reset/:email/:reset_token' do
+  find_email
   # reset the token for this email address
+end
+
+helpers do
+  
+  def find_email
+    @email = Email.find_or_create_by_name(params[:email])
+  end
+
 end
