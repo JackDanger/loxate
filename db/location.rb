d8b2c5682a595f35c18986eb3a8bf9823c2a15f5
Jack Danger Canty <gitcommit@6brand.com>
2008-12-16 00:23:08 -0800

    added location parsing with nickname support

--- db/location.rb
+++ db/location.rb
@@ -14,10 +14,33 @@ class Location < ActiveRecord::Base
     visits.create
   end
 
-  def self.get_coordinates(location)
-    csv = open("maps.google.com/maps/geo?q=#{location}&output=csv&sensor=false&key=#{GOOGLE_MAP_KEY}").read
-    status, accuracy, c1, c2 = csv.to_s.split(',')
-    "#{c1},#{c2}"
+  class << self
+
+    def parse(location)
+      location = location.to_s
+      nickname, address, coordinates = '', '', ''
+
+      return '', '', '' if location =~ /^\s*$/
+
+      # if there is a nickname entered to name this location then extract it
+      if location =~ /(.*)\[(.*)\]$/
+        location, nickname = location.scan(/(.*)\[(.*)\]$/).flatten
+      end
+
+      # if the user has passed us some coordinates then accept them as given
+      # otherwise look them up
+      coordinates = location.=~(/^\s*-?\d+\.\d+,-?\d+\.\d+\s*$/) ?
+        location : geocoordinate(location)
+
+      nickname, address, coordinates
+    end
+
+    def geocoordinate(location)
+      csv = open("maps.google.com/maps/geo?q=#{location}&output=csv&sensor=false&key=#{GOOGLE_MAP_KEY}").read
+      status, accuracy, c1, c2 = csv.to_s.split(',')
+      "#{c1},#{c2}"
+    end
+
   end
 
 end
