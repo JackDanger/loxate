7e97a41a6e32c77a1ae39242bd3ecc115eb76f60
Jack Danger Canty <gitcommit@6brand.com>
2008-12-17 02:57:55 -0800

    trying to test home page form submission.

--- test/application_test.rb
+++ test/application_test.rb
@@ -3,24 +3,41 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 class ApplicationTest < Test::Unit::TestCase
 
   context "viewing the home page" do
-    should "render the index view" do
+    
+    setup do
       get_it '/'
+    end
+    
+    should "render the index view" do
       assert @response.body =~ /<!-- index.haml -->/, @response.body
     end
 
     should "show an email field" do
-      get_it '/'
       assert @response.body =~ /<input.*name='email'/, @response.body
     end
 
     should "show a location field" do
-      get_it '/'
       assert @response.body =~ /<input.*name='location'/, @response.body
     end
 
     should "have a form pointing to /" do
-      get_it '/'
       assert @response.body =~ /<form.*action='\/'/, @response.body
     end
   end
+
+  context "submitting the home page" do
+    
+    setup do
+      post_it "/", "location" => "596 SW Awesome blvd, Seattle, WA", "email" => "me@myself.com"
+    end
+
+    should "render the updated view" do
+      assert @response.body =~ /<!-- updated.haml -->/, @response.body
+    end
+
+    should "save the new location" do
+      assert @response.body =~ /<!-- updated.haml -->/, @response.body
+    end
+  end
+
 end
